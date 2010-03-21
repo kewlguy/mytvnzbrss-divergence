d3114c71587dbde6633eb730c878a0b1febc61e3
kewlguy <kewlguy@hushmail.com>
2010-03-21 07:29:43 -0700

    cleaned up variable names, fixed formatting and cleaned up spelling issues

--- feedgen.php
+++ feedgen.php
@@ -28,11 +28,9 @@ foreach ($id_array as $id_value) {
 	if($id_value != 0) array_push($id_ar, (int)trim($id_value));
 }
 
-/*
-	Search name from show based on ID
-*/
+//	Search name from show based on ID
 for ($z = 0; $z < count($id_ar); $z++) {
-  $namen .= "  ".$show_array[$id_ar[$z]]."<br>"; 
+  $show_names .= "  ".$show_array[$id_ar[$z]]."<br>"; 
 }
 
 // make option readable
@@ -51,59 +49,57 @@ print '<?xml version="1.0" encoding="UTF-8"?>';
 	  <channel>
 		  <title>MyTvNZB Feed v3.0</title>
 			<link><?php echo $domain; ?>index.php?id=<? echo $id; ?></link>
-			<description>MyTvNZB rss - <? echo $x264txt.htmlspecialchars($namen); ?></description>
+			<description>MyTvNZB rss - <? echo $x264txt.htmlspecialchars($show_names); ?></description>
 			<atom:link href="<? echo "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" rel="self" type="application/rss+xml" />
 <?php
 
-/*
-	Start XML  parsing
-*/
-
+// Start XML parsing
 $xml_feed = file_get_contents($tvnzbfeed);
 $xml_parser = xml_parser_create();
-// Porcess
+
+// Process XML data
 xml_parse_into_struct($xml_parser,$xml_feed,$xml_sleutel,$xml_index);
 xml_parser_free($xml_parser);
 
 
-//  This is how an <item> will look like
-function item($titel,$show_id,$link,$description,$datum,$bijlage_url,$bijlage_length,$bijlage_type) {
+// This is how each <item> will look
+function item($show_title,$show_id,$link,$description,$tvnzb_pubdate,$tvnzb_url,$tvnzb_length,$tvnzb_type) {
 	echo "<item>\n";
-	echo "\t<title>".$titel."</title>\n";
-	echo "\t<pubDate>".$datum."</pubDate>\n";
+	echo "\t<title>".$show_title."</title>\n";
+	echo "\t<pubDate>".$tvnzb_pubdate."</pubDate>\n";
 	echo "\t<link>".$link."</link>\n";
 	//  echo "\t<show_id>".$show_id."</show_id>\n";
 	echo "\t<description>".$description."</description>\n";
 	echo "\t<guid isPermaLink=\"false\">".$link."</guid>\n";
-	echo "\t<enclosure url=\"".$bijlage_url."\" length=\"".$bijlage_length."\" type=\"".$bijlage_type."\" />\n";
+	echo "\t<enclosure url=\"".$tvnzb_url."\" length=\"".$tvnzb_length."\" type=\"".$tvnzb_type."\" />\n";
 	echo "</item>\n\n";
 	return true;
 }
 
-// 		Start loop 
+// Start loop to generate <item> elements 
 for($i = 0; !empty($xml_index['TITLE'][$i]); $i++){
 	//  Make variables readable
-  $titel          = htmlspecialchars(trim($xml_sleutel[$xml_index['TITLE'][$i]]['value']));
+  $show_title     = htmlspecialchars(trim($xml_sleutel[$xml_index['TITLE'][$i]]['value']));
   $show_id        = (int)trim($xml_sleutel[$xml_index['SHOW_ID'][$i-1]]['value']);  // little fix
   $link           = $xml_sleutel[$xml_index['LINK'][$i]]['value'];
   $description    = htmlspecialchars($xml_sleutel[$xml_index['DESCRIPTION'][$i]]['value']);
-  $datum          = date("r", strtotime($xml_sleutel[$xml_index['PUBDATE'][$i]]['value']));
-  $bijlage_url    =  $xml_sleutel[$xml_index['LINK'][$i]]['value'];
-  $bijlage_length =  $xml_sleutel[$xml_index['ENCLOSURE'][$i]]["attributes"]['LENGTH'];
-  $bijlage_type   =  $xml_sleutel[$xml_index['ENCLOSURE'][$i]]["attributes"]['TYPE'];
+  $tvnzb_pubdate  = date("r", strtotime($xml_sleutel[$xml_index['PUBDATE'][$i]]['value']));
+  $tvnzb_url    =  $xml_sleutel[$xml_index['LINK'][$i]]['value'];
+  $tvnzb_length =  $xml_sleutel[$xml_index['ENCLOSURE'][$i]]["attributes"]['LENGTH'];
+  $tvnzb_type   =  $xml_sleutel[$xml_index['ENCLOSURE'][$i]]["attributes"]['TYPE'];
   
-  //  ID controle  &&  nuke filter
+  // ID control  &&  nuke filter
   if (in_array($show_id, $id_ar) && !eregi("nuke", $rss_channel["ITEMS"][$i]["TITLE"])) {
     //  Second condition:  x264 optie
-    if ($x264 == "only" AND (eregi("x264", $titel) OR eregi("\.ts", $titel)  )  ) {
+    if ($x264 == "only" AND (eregi("x264", $show_title) OR eregi("\.ts", $show_title)  )  ) {
       //  Print feed:  only X264
-      item($titel,$show_id,$link,$description,$datum,$bijlage_url,$bijlage_length,$bijlage_type);
-    }if ($x264 == "no" AND !(eregi("x264", $titel) OR eregi("\.ts", $titel)  )  ) {
+      item($show_title,$show_id,$link,$description,$tvnzb_pubdate,$tvnzb_url,$tvnzb_length,$tvnzb_type);
+    }if ($x264 == "no" AND !(eregi("x264", $show_title) OR eregi("\.ts", $show_title)  )  ) {
       //  Print feed:  no X264
-      item($titel,$show_id,$link,$description,$datum,$bijlage_url,$bijlage_length,$bijlage_type);
+      item($show_title,$show_id,$link,$description,$tvnzb_pubdate,$tvnzb_url,$tvnzb_length,$tvnzb_type);
     }if ($x264 == "both") {
       //  Print feed:  both x264 and non-x264
-      item($titel,$show_id,$link,$description,$datum,$bijlage_url,$bijlage_length,$bijlage_type);
+      item($show_title,$show_id,$link,$description,$tvnzb_pubdate,$tvnzb_url,$tvnzb_length,$tvnzb_type);
     }
   } //  End ID controle && nuke filter
 } // End For loop
